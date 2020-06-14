4d6a37fb5f2b13eaab1c030919ada524d602676b
Taras Konkevych <tkonkevych@gmail.com>
2020-06-14 15:37:46 +0300

    Added handler, serializer, transport for consuming external messages

---
+++ src/MessageHandler/Command/LogEmojiHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\MessageHandler\Command;
+
+use App\Message\Command\LogEmoji;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+
+class LogEmojiHandler implements MessageHandlerInterface
+{
+    private static $emojis = [
+        'cookie',
+        '🦖dinosaur',
+        'cheese🧀',
+        'robot🤖',
+        '💩poop'
+    ];
+
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function __invoke(LogEmoji $logEmoji)
+    {
+       $index = $logEmoji->getEmojiIndex();
+
+        $emoji = self::$emojis[$index] ?? self::$emojis[0];
+
+        $this->logger->info('Important message! '.$emoji);
+    }
+}
